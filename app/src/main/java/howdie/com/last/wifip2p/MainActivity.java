30bf52f21a1e93462f2aef8040464a2993d13496
adishjain <adish.jain1995@ymail.com>
2017-03-25 22:42:58 +0530

    server code added part2

--- app/src/main/java/howdie/com/last/wifip2p/MainActivity.java
+++ app/src/main/java/howdie/com/last/wifip2p/MainActivity.java
@@ -32,38 +32,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //  Indicates a change in the Wi-Fi P2P status.
-        intentFilter.addAction(WIFI_P2P_STATE_CHANGED_ACTION);
-
-        // Indicates a change in the list of available peers.
-        intentFilter.addAction(WIFI_P2P_PEERS_CHANGED_ACTION);
-
-        // Indicates the state of Wi-Fi P2P connectivity has changed.
-        intentFilter.addAction(WIFI_P2P_CONNECTION_CHANGED_ACTION);
-
-        // Indicates this device's details have changed.
-        intentFilter.addAction(WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
-
-        mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
-        mChannel = mManager.initialize(this, getMainLooper(), null);
-        mReceiver = new WiFiDirectBroadcastReceiver(mManager, mChannel, this);
-
-        mIntentFilter = new IntentFilter();
-        mIntentFilter.addAction(WIFI_P2P_STATE_CHANGED_ACTION);
-        mIntentFilter.addAction(WIFI_P2P_PEERS_CHANGED_ACTION);
-        mIntentFilter.addAction(WIFI_P2P_CONNECTION_CHANGED_ACTION);
-        mIntentFilter.addAction(WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
-        mManager.discoverPeers(mChannel, new WifiP2pManager.ActionListener() {
-            @Override
-            public void onSuccess() {
-
-            }
-
-            @Override
-            public void onFailure(int reasonCode) {
-
-            }
-        });
     }
     @Override
     protected void onResume() {
